e5150b66fdf26f4f27bca503c2b21de8519f8154
ryankawahara <64173139+ryankawahara@users.noreply.github.com>
2021-07-15 13:50:46 -0700

    fixed

--- partyTime.sh
+++ partyTime.sh
@@ -6,6 +6,7 @@
 
 runJoin=false
 runRemove=false
+machineExists=false
 
 while [ $# -gt 0 ] ; do
   case $1 in
@@ -22,10 +23,8 @@ done
 
 for individualGroup in "${groupArray[@]}"
 do
-  # echo "hi"
-
-  # echo $runJoin
   updated="bbnode01"
+  #should be Hostname
 
   /Volumes/SCRATCH/wiretapSDK_2022_MACOSX/tools/MACOSX/x86_64/10_14_6/wiretap_get_metadata -h 192.168.101.10:Backburner -n /servergroups/$individualGroup -s info > serverInfo.xml
   rawData=$(/Volumes/SCRATCH/wiretapSDK_2022_MACOSX/tools/MACOSX/x86_64/10_14_6/wiretap_get_metadata -h 192.168.101.10:Backburner -n /servergroups/$individualGroup -s info)
@@ -35,68 +34,40 @@ do
   updatedServerGroupAttributes=$serverGroupAttributes
 
   serverGrouparray=(${updatedServerGroupAttributes//,/ })
-  # echo ${serverGrouparray[@]}
-  # echo $serverGrouparray
-  # echo ${serverGrouparray[@]}
-  # echo $runJoin
-  # updatedServerGroupAttributes+=","
-  # updatedServerGroupAttributes+=$updated
+
+  for check in ${serverGrouparray[@]}
+  do
+    if [ $check == $updated ]
+    then
+      machineExists=true
+    fi
+  done
+
   if [ $runJoin == true ]
   then
     finalGroupLine=""
-    # echo ${serverGrouparray[@]}
-    # echo ${serverGrouparray[@]}
-    #
-    # echo "${#serverGrouparray[@]}"
-
+    q=0
     for joinElement in ${serverGrouparray[@]}
     do
-      # echo $joinElement
       finalGroupLine+=$joinElement
-      finalGroupLine+=","
-      # echo $finalGroupLine
+      if (( q < ${#serverGrouparray[@]}-1 ))
+      then
+        finalGroupLine+=","
+      fi
+      ((++q))
     done
-    finalGroupLine+=$updated
-
-    # # echo "here"
-    # updatedArray=()
-    # finalGroupLine=""
-    # if [ ${#serverGrouparray[@]} == 0 ]
-    # then
-    #   $updatedArray+="{$updated}"
-    #   finalGroupLine=$updatedArray
-    # else
-    #   n=0
-    #   for element in ${serverGrouparray[@]}
-    #   do
-    #     if (( n < ${#updatedArray[@]} ))
-    #     then
-    #       finalGroupLine+="${element}"
-    #       finalGroupline+=","
-    #       # echo $finalGroupLine
-    #     else
-    #       finalGroupline+="${update}"
-    #     fi
-    #     ((++n))
-    #
-    #   done
-    # fi
-    # # echo $runJoin
-    # echo $finalGroupline
-    # updatedServerGroupAttributes+=","
-    # updatedServerGroupAttributes+=$updated
-  # finalGroupLine="bbmdev"
-
+    if [ $machineExists == false ]
+    then
+      finalGroupLine+=","
+      finalGroupLine+=$updated
+    fi
   fi
 
   if [ $runRemove == true ]
   then
     updatedArray=()
     finalGroupLine=""
-    # echo "remove"
-    # echo ${serverGrouparray[@]}
     i=0
-    # echo "${#serverGrouparray[@]}"
     for element in ${serverGrouparray[@]}
     do
       if [ $element != $updated ]
@@ -105,8 +76,6 @@ do
       fi
       ((++i))
     done
-    # echo ${updatedArray[@]}
-
     j=1
     for groupNameIterator in ${updatedArray[@]}
     do
@@ -115,78 +84,12 @@ do
       then
         finalGroupLine+=","
       fi
-      # echo $j
       ((++j))
     done
-    # echo $finalGroupLine
-
-    # delete=bbmdev
-    # echo ${serverGrouparray[@]/$delete}
-    # newRemovedArray=( "${serverGrouparray[@]/$delete}" )
-    # echo ${newRemovedArray[@]}
-    # echo ${#newRemovedArray[@]}
 
   fi
-  # updatedServerGroupAttributes+=","
   updatedServerGroupAttributes=$finalGroupLine
+  fixedData=${rawData//[$'\t\r\n']}
   xml ed --inplace -u "/info/servers" -v $updatedServerGroupAttributes serverInfo.xml
-
-  # xml ed --inplace -a "/info/servers" --type attr -n servers -v $updated serverInfo.xml
-
-  # echo "hi"+$individualGroup
-
   /Volumes/SCRATCH/wiretapSDK_2022_MACOSX/tools/MACOSX/x86_64/10_14_6/wiretap_set_metadata -h 192.168.101.10:Backburner -n /servergroups/$individualGroup -s info -f serverInfo.xml
 done
-
-# echo $S
-
-# rawData=$(/Volumes/SCRATCH/wiretapSDK_2022_MACOSX/tools/MACOSX/x86_64/10_14_6/wiretap_get_metadata -h 192.168.101.10:Backburner -n /servergroups/group01 -s info)
-
-# updated="bbmdev"
-#
-# /Volumes/SCRATCH/wiretapSDK_2022_MACOSX/tools/MACOSX/x86_64/10_14_6/wiretap_get_metadata -h 192.168.101.10:Backburner -n /servergroups/group01 -s info > serverInfo.xml
-#
-# serverGroupAttributes=$(perl -ne 'print and last if s/.*<servers>(.*)<\/servers>.*/\1/;' <<< "$rawData")
-#
-# updatedServerGroupAttributes=$serverGroupAttributes
-# updatedServerGroupAttributes+=","
-# updatedServerGroupAttributes+=$updated
-#
-# xml ed --inplace -u "/info/servers" -v $updatedServerGroupAttributes serverInfo.xml
-#
-# # xml ed --inplace -a "/info/servers" --type attr -n servers -v $updated serverInfo.xml
-#
-#
-# /Volumes/SCRATCH/wiretapSDK_2022_MACOSX/tools/MACOSX/x86_64/10_14_6/wiretap_set_metadata -h 192.168.101.10:Backburner -n /servergroups/group01 -s info -f serverInfo.xml
-
-
-# echo $rawData
-# echo $HOSTNAME
-# xmlstarlet sel -t -v "count(//servers)" $rawData
-#
-# newData=$(xml ed -u "/info/servers" -v 0 $rawData)
-# echo $newData
-# #
-# serverGroup=$(grep -oPm1 "(?<=<servers>)[^<]+" <<< "$rawData")
-# serverGroup2=$(perl -ne 'print and last if s/.*<servers>(.*)<\/servers>.*/\1/;' <<< "$rawData")
-# # echo "$serverGroup"
-# # echo $rawData
-# echo $serverGroup2
-# read -a attributeArray <<< $rawData
-
-# xmlcatalog --create
-#
-# xmlcatalog --add PUBLIC
-
-# echo "My array: ${attributeArray[2]}"
-#
-# for i in ${attributeArray[@]}
-# do
-#   if [[ $i =~ "server" ]]
-#   then
-#     echo $i | grep -o '<servers>.*</servers>' $i | sed 's/\(<servers>\|<\/servers>\)//g'
-#     echo "Hi+$i"
-#     echo $hello
-#   fi
-#
-# done
