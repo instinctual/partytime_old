d5edb47d566c76579e7a3d3d9b3e4c50812ee30c
Alan Latteri <alan@instinctual.la>
2021-07-30 01:22:51 -0700

    Update partytime_install.sh
    formatting.

--- partytime_install.sh
+++ partytime_install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+INSTALLDIR=/opt/instinctual/partytime/
+
 cd "$(dirname "$0")"
 
 if [ "$EUID" -ne 0 ]
@@ -6,27 +8,14 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-
-
+##We should really check if these are installed, if not prompt.  This force install is not friendly.
 yum install -y epel-release
 yum -y install xmlstarlet
-
-
-
-INSTALLDIR=/opt/instinctual/partytime/
-
-# if [ "$EUID" -ne 0 ]
-#   then echo "Please run as root."
-#   exit
-# fi
 
 mkdir -p $INSTALLDIR
 install -v -m 555 partytimewrapper.sh $INSTALLDIR
 install -v -m 555 partytime.sh $INSTALLDIR
 install -v -m 644 partytime.conf $INSTALLDIR
-
-
-
 install -v -m 555 partytime.desktop /etc/xdg/autostart/partytime.desktop
 install -v -m 444 partytime.service /etc/systemd/system/partytime.service
 
