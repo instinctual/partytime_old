f7405f11ba3e2c42e988990267d7d8dcde900762
Alan Latteri <alan@instinctual.la>
2021-07-30 01:24:15 -0700

    Update partytimewrapper.sh
    formatting.

--- partytimewrapper.sh
+++ partytimewrapper.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 function partyTimeJoin {
-
-  # nohup tuned-adm profile balanced >/dev/null 2>&1 &
-
   nohup /opt/instinctual/partytime/partytime.sh --join >/dev/null 2>&1 &
-
 }
-
 
 /opt/instinctual/partytime/partytime.sh --remove &
 
