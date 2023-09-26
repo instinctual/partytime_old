889f03bfa7839c55b643be9b1cdd27285dadaa82
Alan Latteri <alan@instinctual.la>
2023-09-26 16:15:20 -0700

    use functions

--- partytime.sh
+++ partytime.sh
@@ -44,35 +44,53 @@ while true; do
     
     # Check if the ping was successful
     if [ $? -eq 0 ]; then
-        #echo "Wiretap Ping to $BBMANAGER was successful!"
         break
     else
-        #echo "Wiretap Ping to $BBMANAGER failed. Retrying..."
         sleep 1
     fi
 done
 
+# Get full XML for BBM Group
+bbmgetallxml(){
+    BBGROUPINFO=$(/opt/Autodesk/wiretap/tools/current/wiretap_get_metadata -h $BBMANAGER:Backburner -n /servergroups/$BBGROUP -s info)
+}
+
+# Isolate the server list
+bbmgetserverlistxml(){
+    BBGROUPSERVERS=$(echo "$BBGROUPINFO" | xmlstarlet sel -t -v "/info/servers")
+}
+# Remove the current hostname from the server list
+bbmremovehostxml(){
+    BBGROUPSERVERS=$(echo $BBGROUPSERVERS | sed "s/\b$CURRENTHOST\b//; s/,,/,/; s/^,//; s/,$//")
+}
+
+# Update modified server list into the source XML
+bbmupdateserverlistxml(){
+    BBGROUPINFO=$(echo "$BBGROUPINFO" | xmlstarlet ed --update "/info/servers" --value "$BBGROUPSERVERS")
+}
+
+#Sumbit the modified XML list to Backburner Manager
+bbmsubmitxml(){
+    sleep 1
+    /opt/Autodesk/wiretap/tools/current/wiretap_set_metadata -h $BBMANAGER:Backburner -n /servergroups/$BBGROUP -s info -f /dev/stdin <<<"$BBGROUPINFO"
+    BBGROUPINFO=$(/opt/Autodesk/wiretap/tools/current/wiretap_get_metadata -h $BBMANAGER:Backburner -n /servergroups/$BBGROUP -s info)
+    sleep 1
+}
+
 #Loop thru specified groups and add or remove the host.
 for BBGROUP in "${BBGROUPS[@]}"; do
-    BBGROUPINFO=$(/opt/Autodesk/wiretap/tools/current/wiretap_get_metadata -h $BBMANAGER:Backburner -n /servergroups/$BBGROUP -s info)
+    bbmgetallxml
     if [[ $ACTION == "add" ]]; then
-        BBGROUPSERVERS=$(echo "$BBGROUPINFO" | xmlstarlet sel -t -v "/info/servers")
         # Check if the CURRENTHOST exists in that list
+        bbmgetserverlistxml
         if ! echo $BBGROUPSERVERS | grep -q "\<${CURRENTHOST}\>"; then
             # If not, add the current host to the server XML list
             BBGROUPINFO=$(echo $BBGROUPINFO | xmlstarlet ed --update "/info/servers" -x "concat(.,',${CURRENTHOST}')")
         fi
     elif [[ $ACTION == "remove" ]]; then
-        # Isolate the current server list
-        BBGROUPSERVERS=$(echo "$BBGROUPINFO" | xmlstarlet sel -t -v "/info/servers")
-        # Remove the current hostname from the list
-        BBGROUPSERVERS=$(echo $BBGROUPSERVERS | sed "s/\b$CURRENTHOST\b//; s/,,/,/; s/^,//; s/,$//")
-        # Update modified server list into the XML
-        BBGROUPINFO=$(echo "$BBGROUPINFO" | xmlstarlet ed --update "/info/servers" --value "$BBGROUPSERVERS")
+        bbmgetserverlistxml
+        bbmremovehostxml
+        bbmupdateserverlistxml  
     fi
-    #Sumbit the modified XML list to Backburner Manager
-    sleep 2
-    /opt/Autodesk/wiretap/tools/current/wiretap_set_metadata -h $BBMANAGER:Backburner -n /servergroups/$BBGROUP -s info -f /dev/stdin <<<"$BBGROUPINFO"
-    BBGROUPINFO=$(/opt/Autodesk/wiretap/tools/current/wiretap_get_metadata -h $BBMANAGER:Backburner -n /servergroups/$BBGROUP -s info)
-    sleep 2
+    bbmsubmitxml
 done
