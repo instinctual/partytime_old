89b3f34f73ff0e128bf0c0e4f0773bc3932f2bb7
Alan Latteri <alan@instinctual.la>
2023-09-28 00:37:05 -0700

    test for internet only if xmlstarlet is not installed

--- INSTALL.sh
+++ INSTALL.sh
@@ -13,22 +13,24 @@ if [[ $UID -ne 0 ]]; then
     exit 1
 fi
 
-echo "Testing for Internet connectivity to google.com.  Please wait."
-ping -W2 -c1 google.com > /dev/null
-if [ $? -eq 0 ]
-  then
-    echo "Internet is good.  Moving On."
-    echo
-  else
-    echo "Installer needs Internet connectivity. Open the firewall and try again."
-    exit 0
-fi
+
 
 # Check if xmlstarlet is installed
 if ! rpm -q xmlstarlet &>/dev/null; then
     echo
     echo "xmlstarlet is NOT installed."
-    read -p "Do you want to install xmlstarlet? This will also install epel-release. (y/n) " choice
+    read -p "Do you want to install xmlstarlet? This will also install epel-release and requires an Internet Connection. (y/n) " choice
+
+    echo "Testing for Internet connectivity to google.com.  Please wait."
+    ping -W2 -c1 google.com > /dev/null
+    if [ $? -eq 0 ]
+      then
+        echo "Internet is good.  Moving On."
+        echo
+      else
+        echo "Installer needs Internet connectivity. Open the firewall and try again."
+        exit 0
+    fi
 
     case $choice in
         y|Y)
@@ -53,8 +55,9 @@ fi
   install -m 555 partytimewrapper.sh $INSTALLDIR
   install -m 555 partytime.sh $INSTALLDIR
   install -m 440 partytime.rules /etc/sudoers.d/partytime
+  install -m 444 partytime.desktop /etc/xdg/autostart/partytime.desktop
 
-#Check to see if there is an existing configuration file.  Always copy a .sample for future reference.
+# Check to see if there is an existing configuration file.  Always copy a .sample for future reference.
 if [ ! -f "$INSTALLDIR/partytime.conf" ]
   then
     install -m 664 partytime.conf.sample $INSTALLDIR/partytime.conf
@@ -63,11 +66,27 @@ if [ ! -f "$INSTALLDIR/partytime.conf" ]
     echo "**********************************************************************************************"
 else
   install -m 664 partytime.conf.sample $INSTALLDIR/partytime.conf.sample
-  echo "Existing config file found, not replacing. Adding .sample file."
+  echo "Existing config file found, not going to replace."
 fi
 
-install -m 444 partytime.desktop /etc/xdg/autostart/partytime.desktop
+# Check if the user 'partytime' already exists
+if id "partytime" &>/dev/null; then
+    echo "User 'partytime' already exists."
+  else
+    # Add the user 'partytime' as a system account with no login capabilities and locked account.
+    useradd -M -r -s /sbin/nologin -d /tmp partytime --password '!'
+    # Verify the user was created
+    if id "partytime" &>/dev/null; then
+        echo "User 'partytime' was successfully created."
+      else
+        echo "Failed to create the 'partytime' user."
+        exit 2
+    fi
+fi
+
+# Configure partytime.service
 install -m 444 partytime.service /etc/systemd/system/partytime.service
 systemctl daemon-reload
 systemctl enable --now partytime.service
+
 echo "PartyTime has been installed."
